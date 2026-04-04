3fdb5015eead2f1fbc8f9943a9cf344ae1e2ce80
Julien Bourdeau <julien@julienbourdeau.com>
2026-04-04 18:31:40 +0200

    update to tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://debugbar.dev',
   output: 'static',
-  integrations: [
-    tailwind(),
-  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   markdown: {
     shikiConfig: {
       theme: 'github-light',
